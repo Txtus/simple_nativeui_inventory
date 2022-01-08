44da747902c486a7c49536823b8c73df0c146598
Titus <70706574+Txtus@users.noreply.github.com>
2022-01-08 14:58:55 +0100

    up fx

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,16 +2,11 @@ fx_version 'adamant'
 game 'gta5'
 
 description 'test'
-author 'nativeui'
-version '1'
+author 'Titus#8023'
+version '1.0'
 
 
-server_scripts {'@async/async.lua','@mysql-async/lib/MySQL.lua','sv/*.lua',}
 client_script '@NativeUI/NativeUI.lua'
 client_scripts {'cl/*.lua'}
-shared_scripts {'shared/*.lua'}
 
-ui_page {'html/html.html'}
-
-files {'html/html.html','html/assets/js/*.js','html/assets/css/*.css','html/assets/img/*.png',}
 
